473ed2dc9aa942c310fef07c6563d82f67fb33d7
cesarhc96 <cesarhcmartinez@gmail.com>
2022-03-31 00:09:37 -0700

    palnorte controller added

---
+++ EventSchedule.API/Controllers/PalNorteController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventSchedule.API.Controllers
+{
+    public class PalNorteController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Schedule()
+        {
+            return View();
+        }
+
+        public ActionResult Map()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
